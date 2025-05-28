688cf3732d4df481b7c2a27ef8a5fd816cd33e5c
Nimu-coder <mosotinimrod24@gmail.com>
2025-05-28 16:47:31 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Quantumnim', // Your name
-  ownerNumber: '254108482610, //Your number
+  ownerNumber: '254108482610', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
